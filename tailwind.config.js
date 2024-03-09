8f3855904360be926079eefe473e4cc5e25bd453
datnguyen-nfq <dat.nguyen@nfq.com>
2024-03-10 02:22:33 +0700

    Add home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    colors: {
+      primary: "#F4EEEC",
+      secondary: "#4B6662",
+      white: "#FFFFFF",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
